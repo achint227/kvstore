90df387869e72344b087dd85e926bfc329140fb8
Achint Bhat <abhat29@asu.edu>
2024-07-17 11:55:02 -0700

    implement hset and hget

--- handler.go
+++ handler.go
@@ -5,6 +5,9 @@ import "sync"
 var SETs = map[string]string{}
 var SETsMu = sync.RWMutex{}
 
+var HSETs = map[string]map[string]string{}
+var HSETsMu = sync.RWMutex{}
+
 func set(args []Value) Value {
 	if len(args) != 2 {
 		return Value{typ: "error", str: "ERR wrong number of arguments for 'set' command"}
@@ -38,6 +41,47 @@ func get(args []Value) Value {
 	return Value{typ: "bulk", bulk: value}
 }
 
+func hset(args []Value) Value {
+	if len(args) != 3 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hset' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+	value := args[2].bulk
+
+	HSETsMu.Lock()
+
+	if _, ok := HSETs[hash]; !ok {
+		HSETs[hash] = map[string]string{}
+	}
+
+	HSETs[hash][key] = value
+
+	HSETsMu.Unlock()
+
+	return Value{typ: "string", str: "OK"}
+}
+
+func hget(args []Value) Value {
+	if len(args) != 2 {
+		return Value{typ: "error", str: "ERR wrong number of arguments for 'hget' command"}
+	}
+
+	hash := args[0].bulk
+	key := args[1].bulk
+
+	HSETsMu.RLock()
+	value, ok := HSETs[hash][key]
+	HSETsMu.RUnlock()
+
+	if !ok {
+		return Value{typ: "null"}
+	}
+
+	return Value{typ: "bulk", bulk: value}
+}
+
 func ping(args []Value) Value {
 	return Value{typ: "string", str: "PONG"}
 }
@@ -46,4 +90,6 @@ var Handlers = map[string]func([]Value) Value{
 	"PING": ping,
 	"SET":  set,
 	"GET":  get,
+	"HSET": hset,
+	"HGET": hget,
 }
